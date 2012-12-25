27ab3a8fa4051d16f947ab9150397eea692db35e
Jaafar LOUNES <jlounes@gmail.com>
2012-12-25 17:28:45 +0100

    Premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EagleAds::Application.config.secret_token = 'de23e8447208a8fa225bf2144404378acd6a11140f8d57f9f518ea8f2477629d687e0f85038ce5107cfe4bc3425f8bb177d52e67542769cc9d86220b6ccd9cc9'
